b92c8df83fba2636b6b8780a525ace07faa71900
oscar-monsalve <oscarmonsalvec5@gmail.com>
2024-03-01 13:38:17 -0500

    update

--- lua/om/remap.lua
+++ lua/om/remap.lua
@@ -22,5 +22,7 @@ vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "Q", "<nop>")
 
+-- Subtitute a variable name in the code
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
