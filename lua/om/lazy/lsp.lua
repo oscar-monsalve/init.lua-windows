f112a9442ccc61e5f3aca4cd273daa515f559bc3
oscar-monsalve <oscarmonsalvec5@gmail.com>
2024-07-18 17:20:32 -0500

    update

--- lua/om/lazy/lsp.lua
+++ lua/om/lazy/lsp.lua
@@ -105,7 +105,7 @@ return {
         })
 
         vim.diagnostic.config({
-            update_in_insert = true,
+            update_in_insert = false,
             float = {
                 focusable = false,
                 style = "minimal",
