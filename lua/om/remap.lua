ce62c5967a06590d54aa63b146d406c7e658ce81
oscar-monsalve <oscarmonsalvec5@gmail.com>
2026-06-09 14:25:33 -0500

    updated remaps to match linux ones

--- lua/om/remap.lua
+++ lua/om/remap.lua
@@ -6,12 +6,11 @@
 --   term_mode = "t",
 --   command_mode = "c",
 
--- Define space as leader key
 vim.g.mapleader = " "
-
--- Normal --
--- Exit to file navigation
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Toggle Markview to render .md files
+vim.keymap.set("n", "<leader>vv", ":Markview toggle<CR>")
 
 -- Resize windows with arrows
 vim.keymap.set("n", "<C-Up>", ":resize -2<CR>")
@@ -19,35 +18,40 @@ vim.keymap.set("n", "<C-Down>", ":resize +2<CR>")
 vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>")
 vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>")
 
--- Subtitute a variable name in the code
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+-- Remap j/k to navigate wrapped lines naturally
+-- If no count is given (e.g. 5j), move by visual line (gj/gk)
+-- If a count is given, move by physical line to keep jumps accurate
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+-- Move selected lines up or down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- Put the line below in the current line
 vim.keymap.set("n", "J", "mzJ`z")
+
+-- Move down or up with a centered window view
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
-vim.keymap.set("n", "Q", "<nop>")
+-- greatest remap ever
+vim.keymap.set("x", "<leader>p", [["_dP]])
 
 -- next greatest remap ever : asbjornHaland
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
--- Insert --
--- Crtl-c as Esc
+-- This is going to get me cancelled
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
--- Visual --
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("n", "Q", "<nop>")
 
--- Visual block mode --
--- greatest remap ever
-vim.keymap.set("x", "<leader>p", [["_dP]])
+-- Subtitute a variable name in the code
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
-
-
-
+-- Make file executable
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
