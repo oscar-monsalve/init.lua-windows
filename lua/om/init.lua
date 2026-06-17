04b97bc897cdd605207fbfb6bd0a577bba5282a6
oscar-monsalve <oscarmonsalvec5@gmail.com>
2026-06-17 12:19:56 -0500

    added spelling check

--- lua/om/init.lua
+++ lua/om/init.lua
@@ -12,6 +12,34 @@ function R(name)
     require("plenary.reload").reload_module(name)
 end
 
+vim.filetype.add({
+    extension = {
+        templ = 'templ',
+    }
+})
+
+-- Spell checking
+autocmd({ "FileType", "BufEnter" }, {
+    pattern = "*",
+    callback = function(opts)
+        if not vim.tbl_contains({ "markdown", "txt", "env" }, vim.bo[opts.buf].filetype) then
+            return
+        end
+
+        -- fixes spanish spell checking
+        vim.opt_local.spelllang = { "en_us" }
+        -- vim.opt.spelllang = "es"
+        vim.opt_local.spell = true
+
+        vim.opt_local.linebreak = true
+
+        local ok, cmp = pcall(require, "cmp")
+        if ok then
+            cmp.setup.buffer({ enabled = false })
+        end
+    end,
+})
+
 autocmd('TextYankPost', {
     group = yank_group,
     pattern = '*',
@@ -34,7 +62,8 @@ autocmd('LspAttach', {
     callback = function(e)
         local opts = { buffer = e.buf }
         vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-        vim.keymap.set("n", "K", function() vim.lsp.buf.hover(vim.tbl_deep_extend("force", {}, { border = "rounded" })) end, opts)
+        -- vim.keymap.set("n", "K", function() vim.lsp.buf.hover(vim.tbl_deep_extend("force", {}, { border = "rounded" })) end, opts)
+        vim.keymap.set("n", "K", function() vim.lsp.buf.hover({ border = "rounded" }) end, opts)
         vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help(vim.tbl_deep_extend("force", {}, { border = "rounded" })) end, opts)
         vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
         vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
@@ -45,3 +74,8 @@ autocmd('LspAttach', {
         vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
     end
 })
+
+-- Remove Netrw directory listing when opening nvim in directory
+-- vim.g.netrw_browse_split = 0
+-- vim.g.netrw_banner = 0
+-- vim.g.netrw_winsize = 25
