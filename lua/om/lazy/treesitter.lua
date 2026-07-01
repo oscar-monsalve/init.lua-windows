0260cfbe8f01bdf15081ad5fb80cef216211c878
oscar-monsalve <oscarmonsalvec5@gmail.com>
2026-07-01 10:41:05 -0500

    updated treesitter

--- lua/om/lazy/treesitter.lua
+++ lua/om/lazy/treesitter.lua
@@ -1,43 +1,60 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-        require("nvim-treesitter.configs").setup({
-            -- A list of parser names, or "all"
-            ensure_installed = {"c", "lua", "python", "todotxt", "markdown", "markdown_inline"},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		lazy = false,
+		init = function()
+			local parsers = {
+				"lua",
+				"vim",
+				"vimdoc",
+                "python",
+                "c",
+                "zig",
+                "bash",
+                "latex",
+                "markdown",
+                "markdown_inline",
+                "gitignore",
+                -- "foam",
+			}
 
-            -- Install parsers synchronously (only applied to `ensure_installed`)
-            sync_install = false,
+			local group = vim.api.nvim_create_augroup("omTreesitter", { clear = true })
+			vim.api.nvim_create_autocmd({ "BufEnter", "FileType" }, {
+				group = group,
+				callback = function()
+					if vim.bo.buftype ~= "" then
+						return
+					end
+                    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+					pcall(vim.treesitter.start, 0)
+				end,
+			})
 
-            -- Automatically install missing parsers when entering buffer
-            -- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
-            auto_install = true,
-
-            indent = {
-                enable = true
-            },
-
-            highlight = {
-                -- `false` will disable the whole extension
-                enable = true,
-
-                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                -- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
-                -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                -- Instead of true it can also be a list of languages
-                additional_vim_regex_highlighting = { "markdown" }
-            },
-        })
-
-        local treesitter_parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-        treesitter_parser_config.templ = {
-            install_info = {
-                url = "https://github.com/vrischmann/tree-sitter-templ.git",
-                files = {"src/parser.c", "src/scanner.c"},
-                branch = "master",
-            },
-        }
-
-        vim.treesitter.language.register("templ", "templ")
-    end
+			vim.api.nvim_create_autocmd("User", {
+				group = group,
+				pattern = "VeryLazy",
+				once = true,
+				callback = function()
+					require("nvim-treesitter").install(parsers)
+				end,
+			})
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		lazy = false,
+		config = function()
+			require("nvim-treesitter-textobjects").setup({
+				select = {
+					enable = true,
+					lookahead = true,
+					keymaps = {
+						["af"] = "@function.outer",
+						["if"] = "@function.inner",
+					},
+				},
+			})
+		end,
+	},
 }
